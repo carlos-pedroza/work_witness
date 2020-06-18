12dde4a0a3bb4fdf218b186ba4c6149bd7fa83b5
Carlos Pedroza <carlos.pedrozav@gmail.com>
2020-06-17 19:02:23 -0500

    correction camara and screen adjusts

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAQ8rjWqFNaNkXhWzkpSqntL8svXcoJ2C8")
+    GMSServices.provideAPIKey("AIzaSyBSJRj79KV6JUrEa9SRlEFFVWkXERjpFeM")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
